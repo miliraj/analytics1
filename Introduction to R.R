b350d3983e5cad8469cc1b60f73d0c3e13e3c2c1
unknown <mili7raj@gmail.com>
2018-10-25 14:16:46 +0530

    Introduction to R

---
+++ Introduction to R.R
@@ -0,0 +1,58 @@
+x1 = 3
+x2 <-3
+#Print values
+x1
+x2
+#Assign and Print
+(x3=3)
+#environment----
+#variables in env
+1s()
+# datasets available for use
+data()
+women
+?women 
+?mtcars
+mtcars
+mtcars$mpg
+attach(mtcars)
+mpg
+#libraries currently loaded
+library()
+#Elements
+1s()
+rm(x1)
+x1
+x4 <- 4
+rm(x4)
+x4
+ls(women)
+ls(mtcars)
+?women
+?mean
+x <- c(0:10, 50)
+x
+print
+print(x)
+xm <- mean(x)
+xm
+print(xm)
+c(xm, mean(x, trim = 0.10))
+help(mean)
+1:100
+seq(0,100, by=2)
+seq(0,100 by=3)
+seq(0,100 by3)
+seq(0,100 by=3)
+seq(1,100, by=2)
+seq(1,10,length.out=5)
+x = c(0.10, 50,100)
+x = c(0:10, 50:100)
+x
+x = c(0:10, 50:100)
+rep(c(1,4), times=4)
+rep(c(1,4), times=c(2,6)
+rep(c(1,4), times=c(2,6))
+#clean sole by
+plot(1:10)
+version
